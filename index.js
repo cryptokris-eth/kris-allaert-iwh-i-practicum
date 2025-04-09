8892b00e638fdfc8f9080256ad4ebc39c8ff58ed
KrisCrossDev <github@knowhere.be>
2025-04-09 12:09:56 +0200

    display the list of books

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,11 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const booksUrl = 'https://api.hubspot.com/crm/v3/objects/books';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        const resp = await axios.get(booksUrl, { headers });
+        const data = resp.data.results; // Lijst van Books
+        res.render('books', { title: 'My Personal Library | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching books');    
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -65,6 +81,8 @@ app.post('/update', async (req, res) => {
 
 });
 */
+
+
 
 
 // * Localhost
